22c20351cc9d3a01d151df38671873c8d2750390
Chen Liu <chen.liu.cl2482@yale.edu>
2024-04-28 20:48:57 -0400

    adding license in setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     author_email="xingzhi.sun@yale.edu",
     description="A helper for ploting scatter plots and trajectory plots with plotly",
     keywords="plotly, plotly 3d, 3d plotting, 3d scatter, 3d trajectory",
+    license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/xingzhis/plotly3d',
